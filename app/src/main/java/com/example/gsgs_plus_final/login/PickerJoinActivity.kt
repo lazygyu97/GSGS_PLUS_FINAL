a0b256231f5c6c800c36f008318e31b016b66cd4
lazygyu <park970406@me.com>
2022-06-17 01:14:36 +0900

    픽업할게요 기능 수정 - 리사이클러뷰 교체, 픽커 회원 가입 기능

--- app/src/main/java/com/example/gsgs_plus_final/login/PickerJoinActivity.kt
+++ app/src/main/java/com/example/gsgs_plus_final/login/PickerJoinActivity.kt
@@ -1,12 +1,22 @@
 package com.example.gsgs_plus_final.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.gsgs_plus_final.R
 
 class PickerJoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picker_join)
+
+        val goStudy = findViewById<Button>(R.id.btn_join)
+
+        goStudy.setOnClickListener {
+            val intent = Intent(this, PickerJoinStudyActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_2.kt
+++ app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_2.kt
@@ -1,7 +1,5 @@
 package com.example.gsgs_plus_final.main
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.TextView
-import androidx.recyclerview.widget.AsyncListDiffer
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gsgs_plus_final.R
 import com.example.gsgs_plus_final.adapter.PickUpListAdapter
-import com.example.gsgs_plus_final.login.PickerJoinActivity
 import com.example.gsgs_plus_final.login.PwFindFinishActivity
 import com.example.gsgs_plus_final.vo.pick_list
 
@@ -25,47 +23,58 @@ class HomeFragment_2 : Fragment() {
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v=inflater.inflate(R.layout.fragment_home_2, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_home_2, container, false)
 
         val mainAct = activity as MainActivity
-        val page= v.findViewById<LinearLayout>(R.id.page)
-        val close_btn=v.findViewById<TextView>(R.id.close_btn)
+        val page = v.findViewById<LinearLayout>(R.id.page)
+        val close_btn = v.findViewById<TextView>(R.id.close_btn)
+        val list = v.findViewById<RecyclerView>(R.id.using_list_view)
+
+        // use a linear layout manager
+        val mLayoutManager =  LinearLayoutManager(context);
+        list.setLayoutManager(mLayoutManager);
+
         //픽업요청서 애니메이션
-        val animation_1 = AnimationUtils.loadAnimation(context,R.anim.translate_up)
-        val animation_2 = AnimationUtils.loadAnimation(context,R.anim.translate_down)
+        val animation_1 = AnimationUtils.loadAnimation(context, R.anim.translate_up)
+        val animation_2 = AnimationUtils.loadAnimation(context, R.anim.translate_down)
+        val home= inflater.inflate(R.layout.fragment_home,container,false)
+        val home_bar=home.findViewById<LinearLayout>(R.id.home_bar)
+        val pickList = ArrayList<pick_list>()
 
-
-        val pickList= arrayListOf<pick_list>(
-            pick_list("인천 계양구","서울 강북구"),
-            pick_list("인천 서구","서울 강서구"),
-            pick_list("인천 남구","서울 마포구")
-        )
-
-        val pickAdapter = context?.let { PickUpListAdapter(it,pickList) }
-        val pickUp= v.findViewById<ListView>(R.id.using_list_view)
-        pickUp.adapter=pickAdapter
-
-        //https://soohyun6879.tistory.com/30 참조해서 더 수정해야함
-        pickUp.setOnItemClickListener{parent, view, position, id->
-            if (position==0){
-                pickUp.visibility=View.INVISIBLE
-                mainAct.HideBottomNavi(true)
-                 page.startAnimation(animation_1)
-                page.visibility=View.VISIBLE
-
-                close_btn.setOnClickListener {
-                    pickUp.visibility=View.VISIBLE
-                    mainAct.HideBottomNavi(false)
-                    page.startAnimation(animation_2)
-                    page.visibility=View.INVISIBLE
-                }
-//                activity?.let {
-//                    var intent = Intent(context, PickerJoinActivity::class.java)
-//                    startActivity(intent)
-//                }
-            }
+        pickList.apply {
+            add(pick_list("인천 계양구", "서울 강북구"))
+            add(pick_list("인천 서구", "서울 강서구"))
+            add(pick_list("인천 남구", "서울 마포구"))
         }
+
+        val adapter = PickUpListAdapter(pickList)
+
+        adapter.setOnItemClickListener(object :PickUpListAdapter.OnItemClickListener{
+            override fun onItemClick(data: pick_list, pos: Int) {
+                list.visibility=View.INVISIBLE
+                home_bar.visibility=View.INVISIBLE
+                mainAct.HideBottomNavi(true)
+                page.startAnimation(animation_1)
+                page.visibility=View.VISIBLE
+            }
+        })
+
+        close_btn.setOnClickListener {
+            list.visibility=View.VISIBLE
+            home_bar.visibility=View.VISIBLE
+            mainAct.HideBottomNavi(false)
+            page.startAnimation(animation_2)
+            page.visibility=View.INVISIBLE
+        }
+
+        list.adapter = adapter
+
+
         // Inflate the layout for this fragment
         return v
     }
