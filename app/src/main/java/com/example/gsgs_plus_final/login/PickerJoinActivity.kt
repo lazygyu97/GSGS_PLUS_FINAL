f12c78bcfaaef0319c612d08a528668999b6e0c6
lazygyu <park970406@me.com>
2022-06-17 01:47:55 +0900

    픽업할게요 기능 수정 - 리사이클러뷰 교체, 픽커 회원 가입 기능_2

--- app/src/main/java/com/example/gsgs_plus_final/login/PickerJoinActivity.kt
+++ app/src/main/java/com/example/gsgs_plus_final/login/PickerJoinActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.gsgs_plus_final.R
+import com.example.gsgs_plus_final.main.MainActivity
 
 class PickerJoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +13,16 @@ class PickerJoinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_picker_join)
 
         val goStudy = findViewById<Button>(R.id.btn_join)
-
+        val back =findViewById<Button>(R.id.btn_back)
         goStudy.setOnClickListener {
             val intent = Intent(this, PickerJoinStudyActivity::class.java)
             startActivity(intent)
 
         }
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
