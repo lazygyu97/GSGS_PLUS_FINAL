450b9218770df426b84398ee62dc58ec83aaaf24
lazygyu <park970406@me.com>
2022-06-18 00:40:29 +0900

    픽커 픽업활동 진행 화면 작업

--- app/src/main/java/com/example/gsgs_plus_final/pickUp/DoingPickUpActivity.kt
+++ app/src/main/java/com/example/gsgs_plus_final/pickUp/DoingPickUpActivity.kt
@@ -1,12 +1,22 @@
 package com.example.gsgs_plus_final.pickUp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.gsgs_plus_final.R
 
 class DoingPickUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doing_pick_up)
+
+
+        val btn_finish = findViewById<Button>(R.id.btn_finish)
+
+        btn_finish.setOnClickListener {
+            val intent = Intent(this, FinishPickUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
