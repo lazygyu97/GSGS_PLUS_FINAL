519bbfa8a046e1ce534e994bab8eb214cac0a731
DESKTOP-6NN1F0R\Park Sang Hoon <saog3151@gmail.com>
2022-06-19 04:00:54 +0900

    박상훈: 픽업 요청서 DB 업로드 완료

--- app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_1.kt
+++ app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_1.kt
@@ -1,23 +1,35 @@
 package com.example.gsgs_plus_final.main
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.gsgs_plus_final.R
 import com.example.gsgs_plus_final.pickUp.BeforePickUpActivity
 import com.example.gsgs_plus_final.request.DoingRequestActivity
+import com.example.gsgs_plus_final.vo.PickUpRequest
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import net.daum.mf.map.api.MapView
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class HomeFragment_1 : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fragment_home_1, container, false)
@@ -31,10 +43,16 @@ class HomeFragment_1 : Fragment() {
         val animation_1 = AnimationUtils.loadAnimation(context,R.anim.translate_up)
         val animation_2 = AnimationUtils.loadAnimation(context,R.anim.translate_down)
 
+        val db = Firebase.firestore
+        auth = Firebase.auth
+        val docRef = db.collection("pick_up_request")
+        val docRef2 = db.collection("pickers")
+        val docRef3 = db.collection("users")
+
         val page = v.findViewById<LinearLayout>(R.id.page)
         val pick_up_btn =v.findViewById<Button>(R.id.pick_up_btn)
         val close_btn = v.findViewById<TextView>(R.id.close_btn)
-        val pl_pick = v.findViewById<Button>(R.id.pl_pick)
+        val pl_pick = v.findViewById<Button>(R.id.pick_up_item_requestBtn)
 
         pick_up_btn.setOnClickListener {
             pick_up_btn.visibility=View.INVISIBLE
@@ -51,6 +69,50 @@ class HomeFragment_1 : Fragment() {
         }
 
         pl_pick.setOnClickListener {
+
+            val pick_up_item_name = v.findViewById<EditText>(R.id.pick_up_item_name)
+//            val pick_up_item_img = v.findViewById<ImageView>(R.id.pick_up_item_img)
+            val pick_up_item_addr_start = v.findViewById<EditText>(R.id.pick_up_item_addr_start)
+            val pick_up_item_addr_start_detail = v.findViewById<EditText>(R.id.pick_up_item_addr_start_detail)
+            val pick_up_item_addr_end = v.findViewById<EditText>(R.id.pick_up_item_addr_end)
+            val pick_up_item_addr_end_detaol = v.findViewById<EditText>(R.id.pick_up_item_addr_end_detail)
+            val pick_up_item_request = v.findViewById<EditText>(R.id.pick_up_item_request)
+            val pick_up_item_cost = v.findViewById<EditText>(R.id.pick_up_item_cost)
+
+
+
+            if(pick_up_item_name.text.toString().isNullOrBlank()){
+                Toast.makeText(context,"배송요청 물품이름을 입력하세요!",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if(pick_up_item_addr_start.text.toString().isNullOrBlank()){
+                Toast.makeText(context,"출발지 주소를 입력하세요!",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if(pick_up_item_addr_end.text.toString().isNullOrBlank()){
+                Toast.makeText(context,"도착지 주소를 입력하세요!",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if(pick_up_item_cost.text.toString().isNullOrBlank()){
+                Toast.makeText(context,"예상비용을 입력하세요!",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            docRef3.document(auth.currentUser!!.email.toString()).get().addOnSuccessListener {
+
+                document ->
+                val pick_up_request = PickUpRequest(document.data!!.get("name").toString(),auth.currentUser!!.email.toString(),
+                    document.data!!.get("p_num").toString(),auth.currentUser!!.uid,pick_up_item_name.text.toString(),pick_up_item_addr_start.text.toString()+pick_up_item_addr_start_detail.text.toString()
+                            ,pick_up_item_addr_end.text.toString()+pick_up_item_addr_end_detaol.text.toString()
+                            ,pick_up_item_request.text.toString(),pick_up_item_cost.text.toString(),"0")
+
+                docRef.document(makeRequestUid()).set(pick_up_request)
+
+            }
+
             activity?.let {
                 val intent = Intent(context, DoingRequestActivity::class.java)
                 startActivity(intent)
@@ -62,6 +124,16 @@ class HomeFragment_1 : Fragment() {
         return v
 
 
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun makeRequestUid() : String{
+        var date = Date()
+        var formatter = SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초",Locale("ko","KR"))
+        var formatted = formatter.format(date)
+
+        return formatted
 
     }
 }
--- app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_2.kt
+++ app/src/main/java/com/example/gsgs_plus_final/main/HomeFragment_2.kt
@@ -1,7 +1,9 @@
 package com.example.gsgs_plus_final.main
 
 import android.content.Intent
+import android.graphics.Insets.add
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +21,8 @@ import com.example.gsgs_plus_final.login.PickerJoinActivity
 import com.example.gsgs_plus_final.login.PwFindFinishActivity
 import com.example.gsgs_plus_final.pickUp.BeforePickUpActivity
 import com.example.gsgs_plus_final.vo.pick_list
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment_2 : Fragment() {
 
@@ -30,7 +34,7 @@ class HomeFragment_2 : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         val v = inflater.inflate(R.layout.fragment_home_2, container, false)
 
@@ -38,10 +42,16 @@ class HomeFragment_2 : Fragment() {
         val page = v.findViewById<LinearLayout>(R.id.page)
         val close_btn = v.findViewById<TextView>(R.id.close_btn)
         val list = v.findViewById<RecyclerView>(R.id.using_list_view)
-        val accept =v.findViewById<Button>(R.id.accept)
+        val accept = v.findViewById<Button>(R.id.accept)
         // use a linear layout manager
-        val mLayoutManager =  LinearLayoutManager(context);
+        val mLayoutManager = LinearLayoutManager(context);
         list.setLayoutManager(mLayoutManager);
+
+        //DB접근을 위한 Firebase 객체 선언 부분
+
+        val db = Firebase.firestore
+        val docRef = db.collection("pick_up_request")
+
 
         //픽업요청서 애니메이션
         val animation_1 = AnimationUtils.loadAnimation(context, R.anim.translate_up)
@@ -49,42 +59,108 @@ class HomeFragment_2 : Fragment() {
         val pickList = ArrayList<pick_list>()
 
 
-        pickList.apply {
-            add(pick_list("인천 계양구", "서울 강북구"))
-            add(pick_list("인천 서구", "서울 강서구"))
-            add(pick_list("인천 남구", "서울 마포구"))
-        }
+        //픽업요청서 띄우기(조건 플레그 존재)
+        docRef.whereEqualTo("pick_up_check_flag", "0").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    //픽업 요청서 전부 다 넘어오는지 확인 Lod
+                    Log.d("pick_up_list:", document.data.toString())
+                    // Log.d("documentId:",document.id)
+                    Log.d("addrStart:", document.data["pick_up_item_addr_start"].toString())
+                    Log.d("addrEnd:", document.data["pick_up_item_addr_end"].toString())
 
-        val adapter = PickUpListAdapter(pickList)
+                    val start_addr: String = document.data["pick_up_item_addr_start"].toString()
+                    val end_addr: String = document.data["pick_up_item_addr_end"].toString()
+                    val request_cost: String = document.data["pick_up_item_cost"].toString()
+                    val document_id: String = document.id
 
-        adapter.setOnItemClickListener(object :PickUpListAdapter.OnItemClickListener{
-            override fun onItemClick(data: pick_list, pos: Int) {
-                list.visibility=View.INVISIBLE
-                mainAct.HideBottomNavi(true)
-                page.startAnimation(animation_1)
-                page.visibility=View.VISIBLE
+
+
+
+                    pickList.apply {
+
+                        add(pick_list(start_addr, end_addr, request_cost, document_id))
+
+                    }
+
+                }
+
+                val adapter = PickUpListAdapter(pickList)
+                var accept_doc_id: String = "No id"
+
+                adapter.setOnItemClickListener(object : PickUpListAdapter.OnItemClickListener {
+                    override fun onItemClick(data: pick_list, pos: Int) {
+
+
+                        list.visibility = View.INVISIBLE
+                        mainAct.HideBottomNavi(true)
+                        page.startAnimation(animation_1)
+                        page.visibility = View.VISIBLE
+
+                        v.findViewById<TextView>(R.id.request_addr_start).text = data.addr_start
+                        v.findViewById<TextView>(R.id.request_addr_end).text = data.addr_end
+                        v.findViewById<TextView>(R.id.request_cost).text = data.request_cost
+                        accept_doc_id = data.document_id
+
+                        Log.d("accept_doc_id", accept_doc_id)
+
+                        docRef.document(accept_doc_id).get().addOnSuccessListener { task ->
+                            if (task.data!!.get("pick_up_check_flag") == "1") {
+
+                                Toast.makeText(context,
+                                    "이미 배차된 배송입니다!\n새로고침 해주세요!", Toast.LENGTH_LONG).show()
+                                list.visibility = View.VISIBLE
+                                mainAct.HideBottomNavi(false)
+                                page.startAnimation(animation_2)
+                                page.visibility = View.INVISIBLE
+                                return@addOnSuccessListener
+
+
+                            }
+                        }
+                    }
+                })
+
+                close_btn.setOnClickListener {
+                    list.visibility = View.VISIBLE
+                    mainAct.HideBottomNavi(false)
+                    page.startAnimation(animation_2)
+                    page.visibility = View.INVISIBLE
+                }
+
+                accept.setOnClickListener {
+                    activity?.let {
+
+
+                        docRef.document(accept_doc_id).update("pick_up_check_flag", "1")
+
+                        val intent = Intent(context, BeforePickUpActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                }
+
+                list.adapter = adapter
+
+
+                // Inflate the layout for this fragment
+
+
+            }.addOnFailureListener { exception ->
+                Log.w("No Such document!!!", exception)
             }
-        })
-
-        close_btn.setOnClickListener {
-            list.visibility=View.VISIBLE
-            mainAct.HideBottomNavi(false)
-            page.startAnimation(animation_2)
-            page.visibility=View.INVISIBLE
-        }
-
-        accept.setOnClickListener {
-            activity?.let{
-                val intent = Intent(context, BeforePickUpActivity::class.java)
-                startActivity(intent)
-            }
-
-        }
-
-        list.adapter = adapter
 
 
-        // Inflate the layout for this fragment
+//        pickList.apply {
+//
+//
+////            add(pick_list("인천 계양구", "서울 강북구"))
+////            add(pick_list("인천 서구", "서울 강서구"))
+////            add(pick_list("인천 남구", "서울 마포구"))
+//        }
+
+
         return v
+
     }
 }
