455852ca87d02a4476095654b73b57cc22fbcd05
zeromin1997 <dudalsdl1155@gmail.com>
2022-08-24 22:26:37 +0900

    자동로그인 기능 - 아이디저장, 자동로그인 체크박스 삭제 로그인만 하면 자동으로 로그인되게 설정 로그아웃버튼 눌렀을 때 데이터를 삭제 후 로그인 창

--- app/src/main/java/com/example/gsgs_plus_final/login/LoginActivity.kt
+++ app/src/main/java/com/example/gsgs_plus_final/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.gsgs_plus_final.login
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -21,21 +22,31 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
     private var isDouble = false
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         auth = Firebase.auth
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
 
-
         val db = Firebase.firestore
         val docRef = db.collection("users")
+
+
+        // SharedPreferences 안에 값이 저장되어 있지 않을 때 -> Login
+        if(MySharedPreferences.getUserId(this).isNullOrBlank()
+            || MySharedPreferences.getUserPass(this).isNullOrBlank()) {
+            Toast.makeText(this, "로그인이 필요합니다.", Toast.LENGTH_SHORT).show()
+        }
+        else { // SharedPreferences 안에 값이 저장되어 있을 때 -> MainActivity로 이동
+            Toast.makeText(this, "${MySharedPreferences.getUserId(this)}님 자동 로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
         //로그인 버튼 눌렀을때 메인화면으로 넘어갈때 나오는 토스트 메세지
         val btn_login1 = findViewById<Button>(R.id.btn_login)
@@ -60,6 +71,10 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         //로그인 성공
+
+                        // 로그인 성공 시 아이디와 비밀번호 저장
+                        MySharedPreferences.setUserId(this, login_id.text.toString())
+                        MySharedPreferences.setUserPass(this, login_pwd.text.toString())
 
                         Log.d(TAG, "Success")
                         val user = auth.currentUser
